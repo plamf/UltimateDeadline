400f3936c17c39f76a04d8280dfe2babe70b4969
Franz Plam <franzl@live.at>
2017-10-21 18:45:18 +0200

    version 1 finished

--- UltimateDeadline/src/app/app.component.ts
+++ UltimateDeadline/src/app/app.component.ts
@@ -15,7 +15,6 @@ export class AppComponent implements OnInit {
     this.cbs = document.getElementsByClassName('cbWeek');
   }
 
-
   private generateWeeks() {
     for (let i = 0; i < this.weeks; i++) {
       const el = document.getElementById('weeksList');
@@ -28,6 +27,8 @@ export class AppComponent implements OnInit {
   }
 
   public checkAllUsedWeeks() {
+    this.setDefaults();
+
     const weeksToCheck = this.age * 52;
 
     this.clearCheckedWeeks();
@@ -36,6 +37,17 @@ export class AppComponent implements OnInit {
       const cb = <HTMLInputElement>this.cbs[i];
       cb.checked = true;
     }
+
+    this.generateExplanation();
+  }
+
+  private setDefaults() {
+    if (this.age < 1) {
+      this.age = 1;
+    }
+    if (this.age > 80) {
+      this.age = 80;
+    }
   }
 
   private clearCheckedWeeks() {
@@ -43,5 +55,23 @@ export class AppComponent implements OnInit {
       const cb = <HTMLInputElement>this.cbs[i];
       cb.checked = false;
     }
+  }
+
+  private generateExplanation() {
+
+    const weeksUsed = this.age * 52;
+    const percentUsed = weeksUsed / this.weeks * 100;
+    const slept = weeksUsed / 3.3;
+    const percentSlept = 33;
+    const yearOfDeath = (new Date()).getFullYear() + (80 - this.age);
+
+    const div = document.getElementById('exp');
+    const paragraph = document.getElementById('expPara');
+
+    // tslint:disable-next-line:max-line-length
+    // tslint:disable-next-line:quotemark
+    paragraph.innerHTML = "The average human lifespan lies somewhere around 80 years." + "<br>" + "You used up <strong><u>" + weeksUsed + "</u></strong> weeks already, or <strong><u>" + percentUsed.toFixed(1) + "%</u></strong> of your available time. Approximately <strong><u>" + slept.toFixed(0) + "</u></strong> weeks (<strong><u>" + percentSlept.toFixed(1) + "%</u></strong>) of your life have been spent asleep." + "<br>" + "If you think about doing something (anything, really), you should do it now because you'll most likely be dead sometime around " + "the year <strong><u>" + yearOfDeath + "</u></strong>.<br><br>" + "Have a nice day!";
+
+    div.appendChild(paragraph);
   }
 }
